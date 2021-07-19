06213cc5a607d34358122d68b24484ff9ef94398
Baguette <zhanibek-2005@bk.ru>
2021-07-19 14:49:09 +0600

    jquery asdasdadas

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector(".modal");
-  const modalBtn = document.querySelectorAll("[data-toggle=modal");
-  const switchModal = () => {
-    modal.classList.toggle("modal--visible");
-  };
-  const closeBtn = document.querySelector(".modal__close");
-
-  modalBtn.forEach((element) => {
-    element.addEventListener("click", switchModal);
+$(document).ready(function () {
+  var modal = $(".modal"),
+    modalBtn = $("[data-toggle=modal]"),
+    closeBtn = $(".modal__close");
+  modalBtn.on("click", function () {
+    modal.toggleClass("modal--visible");
   });
-  closeBtn.addEventListener("click", switchModal);
+  closeBtn.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
 });
