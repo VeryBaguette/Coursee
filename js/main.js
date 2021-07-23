7d4cd65842e882912a4d4350099db876b8b51e7f
Baguette <zhanibek-2005@bk.ru>
2021-07-23 18:15:24 +0600

    sd

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
   modalBtn.on("click", function () {
     modal.toggleClass("modal--visible");
   });
+
   closeBtn.on("click", function () {
     modal.toggleClass("modal--visible");
   });
+
   var mySwiper = new Swiper(".swiper-container", {
     loop: true,
     pagination: {
@@ -19,11 +21,37 @@ $(document).ready(function () {
       prevEl: ".swiper-button-prev",
     },
   });
+
   var next = $(".swiper-button-next");
   var prev = $(".swiper-button-prev");
   var bullets = $(".swiper-pagination");
 
-  next.css("left", prev.width() + pagination.width());
-
   new WOW().init();
+  $(".modal__form").validate({
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2,
+      },
+      userEmail: {
+        required: true,
+        email: true,
+      },
+      userPhone: "required",
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя должно быть не короче двух букв",
+      },
+      userEmail: {
+        required: "Адрес электронной почты обязателен",
+        email: "Введите адрес электронной почты в формате: name@domain.com",
+      },
+      userPhone: "Номер телефона обязателен",
+    },
+  });
+  $("[type=tel]").mask("+7(000) 000-00-00", {
+    placeholder: "+7(___) ___-__-__",
+  });
 });
