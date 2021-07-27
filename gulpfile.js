90fc7d822d0f8b28e1991bc6dc3fa5858a29038c
Baguette <zhanibek-2005@bk.ru>
2021-07-27 18:44:37 +0600

    optimized

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, series } = require("gulp");
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
 const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const minify = require("gulp-minify");
+const htmlmin = require("gulp-htmlmin");
+const tinypng = require("gulp-tinypng-compress");
 
 function bs() {
   serveSass();
@@ -29,4 +33,52 @@ function serveSass() {
     .pipe(browserSync.stream());
 }
 
+function buildCSS(done) {
+  src("./css/**/**.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(dest("dist/css"));
+  done();
+}
+
+function buildJS(done) {
+  src(["js/**.js", "!js/**.min.js"])
+    .pipe(minify({ ext: { min: ".js" } }))
+    .pipe(dest("dist/js"));
+  src("js/**.min.js").pipe(dest("dist/js"));
+  done();
+}
+
+function buildHTML(done) {
+  src("**.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("dist/"));
+  done();
+}
+
+function buildPHP(done) {
+  src("**.php").pipe(dest("dist/"));
+  src("./phpmailer/**/**").pipe(dest("dist/phpmailer"));
+  done();
+}
+
+function buildFONTS(done) {
+  src("./fonts/**/**").pipe(dest("dist/fonts"));
+  done();
+}
+
+function imgMin(done) {
+  src("./img/**/**")
+    .pipe(tinypng({ key: "9QKbkf5ssJSmvvpGJv00tlWzDTRwQywY" }))
+    .pipe(gulp.dest("dist/img"));
+  src("./img/**/**.svg").pipe(gulp.dest("dist/img"));
+  done();
+}
 exports.default = bs;
+exports.build = series(
+  buildCSS,
+  buildJS,
+  buildHTML,
+  buildPHP,
+  buildFONTS,
+  imgMin
+);
